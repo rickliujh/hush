365e03846a804264fe96563c75f93b96e2eeba04
Rick Liu <richard_y2019@163.com>
2022-02-18 02:11:04 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rickliujh/hush
+
+go 1.16
--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package hush
+
+type Dispatcher struct {
+}
+
+type State string
+
+type From string
+
+type Subject string
+
+type Action struct {
+	From    From
+	Subject Subject
+	Object  interface{}
+	State   State
+}
+
+type Task func() error
+
+type ActionGroup []Action
+
+func (d *Dispatcher) On(action ActionGroup, task Task) *Dispatcher {
+	panic("not implement")
+}
